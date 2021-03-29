0db6e9510daab1bcabacdfe2f1c7e60c5272d973
BRASS <mr.brn.lewis@outlook.com>
2021-03-30 00:53:16 +0800

    further work on quiz handling features

--- Assets/script.js
+++ Assets/script.js
@@ -14,28 +14,27 @@ $('.carousel').carousel({
       if(event.target.type == "button")
       {
         answerID = event.target.id;
-        answer.push(answerID.substring(answerID.indexOf("-")+1));
-        console.log(answer);
+        answer.push(answerID.substring(answerID.indexOf("-")+1)); //return the number of btn element which = answer chosen
+        if(answer.length < 5)
+        {
         $('.carousel').carousel("next")
+        }
+        else
+        {
+            endQuiz();
+        }
       }
   });
 
-
-
-function hideCardID()
-{
-
-}
-
-
-function nextQuestion()
-{
-
-
-}
-
+  $('#carouselQuiz').on('slide.bs.carousel', function (e) {
+  })
 
 function timer()
 {
     
+}
+
+function endQuiz()
+{
+    let answers = ["2","4","1","3","2"];
 }
