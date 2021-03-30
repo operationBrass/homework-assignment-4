495a534ec4a69cca82e4534e294746c448bc70f4
BRASS <mr.brn.lewis@outlook.com>
2021-03-30 16:54:50 +0800

    adding timer partially working

--- Assets/script.js
+++ Assets/script.js
@@ -85,3 +85,19 @@ subName.addEventListener("click",function() {
   }
 
 });
+
+
+
+//count down timer
+setInterval(() => {
+  
+  var d = new Date(); //get current time
+  var seconds = d.getMinutes() * 60 + d.getSeconds(); //convet current mm:ss to seconds for easier caculation, we don't care hours.
+  var fiveMin = 60 * 5; //five minutes is 300 seconds!
+  var timeleft = fiveMin - seconds % fiveMin; // let's say now is 01:30, then current seconds is 60+30 = 90. And 90%300 = 90, finally 300-90 = 210. That's the time left!
+  var result = parseInt(timeleft / 60) + ':' + timeleft % 60; //formart seconds back into mm:ss 
+  console.log(document.getElementById('timer').innerHTML = "Time to go: " + result);
+
+}, 500);
+
+
